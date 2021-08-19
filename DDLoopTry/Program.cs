3f91ca6610400f5224ff0a3408eed6cd8c5b0a48
Danylo <danyloboshagora@gmail.com>
2021-08-19 10:25:50 +0200

    check1

---
+++ DDLoopTry/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DDLoopTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool one = true;
+            bool two = true;
+            
+            int selectionLevel1 = 0;
+
+            while (selectionLevel1 == 0)
+            {
+                switch (selectionLevel1)
+                {
+                    
+                }
+                Pr("1. Main");
+                Pr("2. Movies");
+                Pr("3. Delete");
+                Pr("4. Edit");
+                Pr("5. Remove");
+                selectionLevel1 = Int32.Parse(Console.ReadLine());
+            }
+        }
+
+        private static void Pr(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+    }
+}
---
+++ WeMakeSoft_extentionVideo_Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WeMakeSoft_extentionVideo_Models
+{
+    public class Class1
+    {
+    }
+}
